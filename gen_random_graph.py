ace3823a459c40c2eab375ebc9e4f4bf3e5d8131
Marisa Yeung <marisayeung1@gmail.com>
2014-11-27 21:58:30 -0800

    Changed output format. Output file is also now binary.

--- gen_random_graph.py
+++ gen_random_graph.py
@@ -2,25 +2,42 @@
 
 from random import sample
 from random import randint
+from struct import pack
+from sys import stdout
 
-edges = 1000
-nodes = 16384
+edges = 1000	#max edges per node (degree)
+nodes = 16384	#total num nodes
 
-print("node_id,adj_list")
 
-line = ""
-for i in range(0,nodes):						# range is [a,b)
+# write out the number of nodes
+b = pack("@i", nodes)
+stdout.write(str(b))
+
+# write out the cumulative degree of each node
+first = randint(1,edges)
+a = [first]
+sum = first
+b = pack("@i",sum)
+stdout.write(str(b))
+
+for i in range(1,nodes):						# range is [a,b)
 	num_edges = randint(1,edges)
-	line = str(i) + ","
-	adj = sample(range(0,nodes),num_edges)		# sample chooses without replacement				
+	sum += num_edges
+	a.append(num_edges)
+	b = pack("@i",sum)
+	stdout.write(str(b))
+
+for i in a:
+	adj = sample(range(0,nodes),a[i])			# sample chooses without replacement
 	for j in adj:
 		# don't add itself to adj list
 		if j != i:
-			line += str(j) + " "
+			b = pack("@i",j)
+			stdout.write(str(b))
 		else:
 			# try one more time to generate a unique neighbor
 			s = randint(0,nodes)
 			if not (s in adj):
-				line += str(s) + " "
-
-	print(line)
+				b = pack("@i",s)
+				stdout.write(str(b))
+print ""
