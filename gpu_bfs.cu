29783b967c2212d5e3202ec597ca01eb4d2c3c66
Marisa Yeung <marisayeung1@gmail.com>
2014-12-01 08:01:43 -0800

    Adding some error handling and comments

--- gpu_bfs.cu
+++ gpu_bfs.cu
@@ -9,41 +9,44 @@ using namespace std;
 
 #include "bfs_kernel.cu"
 
+/*
+ * gpu_bfs.cu
+ *
+ *	Usage: ./executable <graph_file> <output file>
+ *
+ *	Input: Name of the file containing the graph. Expected format
+ *			is binary with the following data (with no newlines)
+ *			number of nodes (4 bytes)
+ *			cumulative list of node degrees (4 bytes * numer of nodes)
+ *			edge list (4 bytes * number of links)
+ *			
+ *			example: 4 nodes; node 0 edges: 1,2; node 1 edges: 0; node 1 edges: 3
+ *			4
+ *			0,2,3
+ *			1,2,0,3
+ *			written in binary with no newlines or commas
+ *
+ *	Output:
+ *		stdout: Time it took kernel to run, in ms
+ *		output file: Each node and its distance from starting node
+ *					(0) cost:0
+ *					(1) cost:3
+ *		
+ */
+
+
 const int MAX_THREADS_PER_BLOCK = 256;
 char *infile = NULL;
-
-void usage(char* prog_name, const char* more)
-{
-	cerr << more;
-	cerr << "usage: " << prog_name << "input_file " << endl;
-	exit(0);
-}
-
-void parse_args(int argc, char** argv) 
-{
-	for (int i = 0; i < argc; i++)
-	{
-		if (argv[i][0] == '-')
-		{
-			switch(argv[i][1])
-			{
-				case 'i':
-					if (i == argc - 1) {
-						usage(argv[0], "Infile missing");
-					}
-					infile = argv[i + 1];
-					i++;
-					break;
-			}
-		}
-	}
-}
 
 void bfsGraph(char *filename, int start_position, char *outFile)
 {
 	int nb_nodes;
-	ifstream finput;
-	finput.open(filename, ios::in | ios::binary);
+	ifstream finput(filename, ios::in | ios::binary);
+	if(!finput.is_open())
+	{
+		cerr << "Unable to open file" << endl;
+		exit(EXIT_FAILURE);
+	}
 
 	// Read number of nodes, first 4 bytes of file
 	finput.read((char*)&nb_nodes, 4);
@@ -66,19 +69,6 @@ void bfsGraph(char *filename, int start_position, char *outFile)
 	int *links = new int[nb_links];
 	finput.read((char*) links, nb_links * 4);
 	finput.close();
-
-	//cout << "Number of nodes: " << nb_nodes << endl;
-	//cout << "Number of links: " << nb_links << endl;
-
-	// Determine number of blocks and threads
-	int num_of_blocks = 1;
-	int num_of_threads_per_block = nb_nodes;
-	if (nb_nodes > MAX_THREADS_PER_BLOCK)
-	{
-		num_of_blocks = (int) ceil((double) nb_nodes/
-			(double) MAX_THREADS_PER_BLOCK);
-		num_of_threads_per_block = MAX_THREADS_PER_BLOCK;
-	}
 
 	// allocate host memory
 	Node *h_graph_nodes = (Node *) malloc(sizeof(Node) * nb_nodes);
@@ -138,10 +128,21 @@ void bfsGraph(char *filename, int start_position, char *outFile)
 	cudaMemcpy(d_cost, h_cost, sizeof(int) * nb_nodes,
 		cudaMemcpyHostToDevice);
 
+	// Determine number of blocks and threads
+	int num_of_blocks = 1;	// at least 1
+	int num_of_threads_per_block = nb_nodes;
+	if (nb_nodes > MAX_THREADS_PER_BLOCK)
+	{
+		num_of_blocks = (int) ceil((double) nb_nodes/
+			(double) MAX_THREADS_PER_BLOCK);
+		num_of_threads_per_block = MAX_THREADS_PER_BLOCK;
+	}
+
 	bool *d_over;
 	cudaMalloc((void **) &d_over, sizeof(bool));
 	bool stop;
 
+	// Call the kernel at each level iteration
 	struct timeval start, end;    
 	gettimeofday(&start, NULL);
 	do 
@@ -157,12 +158,11 @@ void bfsGraph(char *filename, int start_position, char *outFile)
 
 		cudaMemcpy(&stop, d_over, sizeof(bool),
 			cudaMemcpyDeviceToHost);
-		//cout << "stop : " << stop << endl;
-		//stop = false;
+
 	} while(stop);
 
 	gettimeofday(&end, NULL);
-	printf("%d nodes: %ld\n", nb_nodes,
+	printf("%ld\n",
            (end.tv_sec * 1000000 + end.tv_usec)
            - (start.tv_sec * 1000000 + start.tv_usec));
 	
@@ -192,9 +192,6 @@ void bfsGraph(char *filename, int start_position, char *outFile)
 	fclose(fpt);
 	*/
 
-	
-	
-
 	// Store results into a file
 	FILE *fpo = fopen(outFile, "w");
 	for (int i = 0; i < nb_nodes; i++)
@@ -219,9 +216,14 @@ void bfsGraph(char *filename, int start_position, char *outFile)
 
 int main(int argc, char **argv)
 {
-	//parse_args(argc, argv);
+	if (argc != 3)
+	{
+		printf("Usage: %s <input file> <output file>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 	char *filename = argv[1];
 	char*outFile = argv[2];
-	bfsGraph(filename, 0, outFile);
+	int starting_node_id = 0;
+	bfsGraph(filename, starting_node_id, outFile);
 	return 0;
 }
